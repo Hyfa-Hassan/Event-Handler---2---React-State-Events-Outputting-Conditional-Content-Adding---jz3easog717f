4853ec6e2aea23cb08f6ff6e7f57431efc579433
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-17 20:25:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ const App = () => {
 
   const handleClick = (event) =>{
     // use console.log
-    console.log(event.target.value)
+    console.log(event)
   }
 
   // do not remove the two buttons or change their id
